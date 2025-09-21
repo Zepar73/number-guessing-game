f76f8ade02daab171c8db407a0a0164b67a1741b
Victor Alba <victoralba0798@gmail.com>
2025-09-21 21:48:36 +0900

    Added what user will see when first starting game

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Welcome to the Number Guessing Game!\n";
+    std::cout << "I'm thinking of a number between 1 and 100.\n";
+    std::cout << "You have a se number of chances to guess the correct number based on difficulty\n";
+
+    std::cout << "Please select the difficulty level: \n";
+    
+
+
+
+
+
+    return 0;
+}
