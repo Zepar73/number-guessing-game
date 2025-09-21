0bfc4924ee689305dafc1dc25ea0c0fd4b3d61af
Victor Alba <victoralba0798@gmail.com>
2025-09-21 22:07:41 +0900

    Started on Difficulty level and chances for game

--- main.cpp
+++ main.cpp
@@ -2,15 +2,23 @@
 
 int main(){
 
+      int level;
     std::cout << "Welcome to the Number Guessing Game!\n";
     std::cout << "I'm thinking of a number between 1 and 100.\n";
     std::cout << "You have a se number of chances to guess the correct number based on difficulty\n";
 
     std::cout << "Please select the difficulty level: \n";
-    
+    std::cout << "1. Easy (10 chances)\n";
+    std::cout << "2. Medium (5 chances\n)";
+    std::cout << "3. Hard (chances)\n";
+    std::cout << "Enter your choice: ";
+    std::cin>> level;
 
-
-
+    switch(level){
+        case 1: std::cout <<"You haave chosen Easy difficulty.\n";
+            break;
+    }
+    std::cout << "Let's start the game!";
 
 
     return 0;
